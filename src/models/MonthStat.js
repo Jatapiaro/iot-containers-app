19cbf92f8fda32ae731f87a3509cdb231196ed32
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-08-12 17:54:45 -0500

    IOT-23 remueve codigo innecesario

--- src/models/MonthStat.js
+++ src/models/MonthStat.js
@@ -6,18 +6,6 @@ export default class MonthStat {
     }
 
     /**
-     * Initialize the data. This stat needs 23 slots
-     * and 23 labels that matches with each hour of the day
-     */
-    initData() {
-        for (let i = 0; i <= 23; i++) {
-            let label = (i < 10)? `${i}0:00` : `${i}:00`;
-            this.labels.push(label);
-            this.data.push(0);
-        }
-    }
-
-    /**
      * Converts the stat model to an object to be passed
      * to the chart library
      */
--- src/models/WeekStat.js
+++ src/models/WeekStat.js
@@ -4,19 +4,7 @@ export default class WeekStat {
         this.labels = ['DOM','LUN','MAR','MIE','JUE','VIE','SAB'];
         this.data = [0,0,0,0,0,0,0];
     }
-
-    /**
-     * Initialize the data. This stat needs 23 slots
-     * and 23 labels that matches with each hour of the day
-     */
-    initData() {
-        for (let i = 0; i <= 23; i++) {
-            let label = (i < 10)? `${i}0:00` : `${i}:00`;
-            this.labels.push(label);
-            this.data.push(0);
-        }
-    }
-
+    
     /**
      * Converts the stat model to an object to be passed
      * to the chart library
